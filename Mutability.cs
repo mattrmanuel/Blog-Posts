b0248d71399d87c9c97cf3df9f6e32505f004592
Matthew Manuel <55068994+mattrmanuel@users.noreply.github.com>
2019-10-09 20:41:46 -0500

    Add files via upload
    Code accompaniment for "Mutable and Immutable Collections"

---
+++ Mutability.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Fred";
+            // name[0] = "K"; --> throws error
+
+            Console.WriteLine(name); // Fred
+
+            List<int> newList = new List<int>() { 1, 2, 3 };
+
+            newList[0] = 5;
+
+            Console.WriteLine(newList[0]); // Prints "5"
+
+            newList.Add(12);
+
+            Console.WriteLine("The size of the list is {0} and the last " +
+                "element is {1}", newList.Count,newList[newList.Count-1]);
+
+            newList.RemoveAt(3);
+
+            Console.WriteLine("The size of the list is {0} and the last " +
+                "element is {1}", newList.Count, newList[newList.Count - 1]);
+
+        }
+    }
+}
